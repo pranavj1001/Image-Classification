524d733038eab2b84eb9754e18a71fb2e06e315b
pranavj1001 <pranavj1001@gmail.com>
2018-01-08 18:54:55 +0530

    import Libraries

---
+++ CNN_Classification.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  8 14:04:25 2018
+
+@author: pranavjain
+"""
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
