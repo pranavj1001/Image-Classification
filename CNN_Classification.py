2f78286950e07f1e746f0d8d46663f9cba9eca30
pranavj1001 <pranavj1001@gmail.com>
2018-01-08 18:57:03 +0530

    design a CNN and compile it

--- CNN_Classification.py
+++ CNN_Classification.py
@@ -11,3 +11,28 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+# make 32 feature detectors with a size of 3x3
+# choose the input-image's format to be 64x64 with 3 channels
+classifier.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation="relu"))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+classifier.add(Dense(activation="relu", units=128))
+classifier.add(Dense(activation="sigmoid", units=1))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
