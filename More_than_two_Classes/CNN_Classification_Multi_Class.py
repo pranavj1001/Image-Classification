4b37327059f1d4bf1e82c405048b64a840590b6d
pranavj1001 <pranavj1001@gmail.com>
2018-03-01 20:02:23 +0530

    design the CNN

--- More_than_two_Classes/CNN_Classification_Multi_Class.py
+++ More_than_two_Classes/CNN_Classification_Multi_Class.py
@@ -12,3 +12,29 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Convolution
+# make 32 feature detectors with a size of 3x3
+# choose the input-image's format to be 64x64 with 3 channels
+classifier.add(Conv2D(32, (3, 3), input_shape=(128, 128, 3), activation="relu"))
+
+# Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Flattening
+classifier.add(Flatten())
+
+# Full connection
+classifier.add(Dense(activation="relu", units=128))
+# make sure the units is equal to the number of classes
+classifier.add(Dense(activation="softmax", units=10))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'rmsprop', loss = 'categorical_crossentropy', metrics = ['accuracy'])
